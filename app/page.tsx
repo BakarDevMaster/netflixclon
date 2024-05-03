b3607a57a8d97e6dc2659ce1af90b38eabb7986b
BakarDevMaster <ma7260712@gmail.com>
2024-05-03 12:41:23 +0500

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Netflix from "./Netflix/page";
 
+
 export default function Home() {
   return (
      <Netflix/>
+
+
+  
+ 
+  
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        'gradient': 'animateBg 6s linear infinite',
       },
+      backgroundImage: {
+        'gradient': 'linear-gradient(90deg, #cf5c5c, #c19b4a, #def2a0, #c6ee4a, #42eca6, #208ea2, #cf5c5c, #c19b4a)',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundSize: {
+        '700': '700% 100%',
+      },
+      keyframes: {
+        animateBg: {
+          '0%': { 'background-position': '0% 0%' },
+          '100%': { 'background-position': '100% 0%' },
+        }
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
+
